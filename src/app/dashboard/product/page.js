d150aae63edbc6be03299d90e8cc5f39277ddd64
sakshi wankhede <sakwan132000@gmail.com>
2024-08-25 20:57:34 +0530

    key passed

--- src/app/dashboard/product/page.js
+++ src/app/dashboard/product/page.js
@@ -1,11 +1,8 @@
 import React from 'react'
-// import Product from './Product'
 import UpdatedProduct from './updatedProduct'
-// import NewProduct from './nerProduct'
 
 function Page() {
   return (
-    // <NewProduct />
     <UpdatedProduct />
   )
 }
